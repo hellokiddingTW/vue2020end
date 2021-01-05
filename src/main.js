f15fc34c8749a7521bb0b9f096e1e84cc4a839e0
Nate Yeh <a498390344@gmail.com>
2021-01-06 01:24:46 +0800

    second

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
+
 import axios from 'axios';//主要的Ajax套件
 import VueAxios from 'vue-axios'; //將它轉為vue的套件
 import router from './router';
--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import VueRouter from 'vue-router'
 //自訂的元件
 import Home from '@/components/HelloWorld';
 import Page from '@/components/pages/page';
-import Login from'@/components/pages/login';
+import Login from'@/components/pages/Login';
 import card2 from'@/components/pages/child2';
 import card3 from'@/components/pages/child3';
 import Menu from'@/components/pages/menu';
